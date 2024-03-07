bb42ae34eb3e469d0694a9aad2086604d7ac17dd
BagusRezky <bagusrezky15@gmail.com>
2024-03-07 17:04:44 +0700

    first commit

---
+++ lib/styles/color_style.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class ColorStyle {
+  ColorStyle._();
+  static const Color background = Color(0xffF9F9F9);
+  static const Color blue = Color(0xff178AF3);
+  static const Color secondary = Color(0xff6c757d);
+  static const Color greenlight = Color(0xffCDD971);
+  static const Color greendark = Color(0xff3D5920);
+  static const Color red = Color(0xffE22323);
+  static const Color warning = Color(0xffffc107);
+  static const Color info = Color(0xff17a2b8);
+  static const Color light = Color(0xfff8f9fa);
+  static const Color dark = Color(0xff343a40);
+  static const Color white = Color(0xffffffff);
+  static const Color pink = Color(0xffFFD2C0);
+  static const Color purple = Color(0xffF8D9DF);
+  static const Color black = Color(0xff000000);
+}
---
+++ lib/styles/font_style.dart
@@ -0,0 +1,67 @@
+import 'dart:ui';
+
+import 'package:google_fonts/google_fonts.dart';
+
+class GoogleTextStyle {
+  static final fw100 = GoogleFonts.josefinSans(
+    fontWeight: FontWeight.w100,
+  );
+  static final fw200 = GoogleFonts.josefinSans(
+    fontWeight: FontWeight.w200,
+  );
+  static final fw300 = GoogleFonts.josefinSans(
+    fontWeight: FontWeight.w300,
+  );
+  static final fw400 = GoogleFonts.josefinSans(
+    fontWeight: FontWeight.w400,
+  );
+  static final fw500 = GoogleFonts.josefinSans(
+    fontWeight: FontWeight.w500,
+  );
+  static final fw600 = GoogleFonts.josefinSans(
+    fontWeight: FontWeight.w600,
+  );
+  static final fw700 = GoogleFonts.josefinSans(
+    fontWeight: FontWeight.w700,
+  );
+  static final fw800 = GoogleFonts.josefinSans(
+    fontWeight: FontWeight.w800,
+  );
+  static final fw900 = GoogleFonts.josefinSans(
+    fontWeight: FontWeight.w900,
+  );
+  static final fwBold = GoogleFonts.josefinSans(
+    fontWeight: FontWeight.bold,
+  );
+
+  static final po100 = GoogleFonts.poppins(
+    fontWeight: FontWeight.w100,
+  );
+  static final po200 = GoogleFonts.poppins(
+    fontWeight: FontWeight.w200,
+  );
+  static final po300 = GoogleFonts.poppins(
+    fontWeight: FontWeight.w300,
+  );
+  static final po400 = GoogleFonts.poppins(
+    fontWeight: FontWeight.w400,
+  );
+  static final po500 = GoogleFonts.poppins(
+    fontWeight: FontWeight.w500,
+  );
+  static final po600 = GoogleFonts.poppins(
+    fontWeight: FontWeight.w600,
+  );
+  static final po700 = GoogleFonts.poppins(
+    fontWeight: FontWeight.w700,
+  );
+  static final po800 = GoogleFonts.poppins(
+    fontWeight: FontWeight.w800,
+  );
+  static final po900 = GoogleFonts.poppins(
+    fontWeight: FontWeight.w900,
+  );
+  static final poBold = GoogleFonts.poppins(
+    fontWeight: FontWeight.bold,
+  );
+}
